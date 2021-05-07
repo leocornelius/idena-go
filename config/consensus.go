f4eed4304feb4d5fde291afe09af312cb69e3fdf
sidenaio <st@idena.io>
2021-05-07 11:06:37 +0500

    Introduce validation sharding

--- config/consensus.go
+++ config/consensus.go
@@ -55,6 +55,7 @@ type ConsensusConf struct {
 	EncourageEarlyInvitations         bool
 	EnableDelayedOfflinePenalty       bool
 	BurnInviteeStake                  bool
+	EnableValidationSharding          bool
 }
 
 type ConsensusVerson uint16
@@ -67,12 +68,16 @@ const (
 
 	// Enables events sorting
 	ConsensusV5 ConsensusVerson = 5
+
+	// Enables validation sharding
+	ConsensusV6 ConsensusVerson = 6
 )
 
 var (
 	v3                ConsensusConf
 	v4                ConsensusConf
 	v5                ConsensusConf
+	v6                ConsensusConf
 	ConsensusVersions map[ConsensusVerson]*ConsensusConf
 )
 
@@ -123,6 +128,10 @@ func init() {
 	v5 = v4
 	ApplyConsensusVersion(ConsensusV5, &v5)
 	ConsensusVersions[ConsensusV5] = &v5
+
+	v6 = v5
+	ApplyConsensusVersion(ConsensusV6, &v6)
+	ConsensusVersions[ConsensusV6] = &v6
 }
 
 func ApplyConsensusVersion(ver ConsensusVerson, cfg *ConsensusConf) {
@@ -149,6 +158,11 @@ func ApplyConsensusVersion(ver ConsensusVerson, cfg *ConsensusConf) {
 		cfg.GenerateGenesisAfterUpgrade = true
 		cfg.StartActivationDate = time.Date(2021, 05, 11, 8, 0, 0, 0, time.UTC).Unix()
 		cfg.EndActivationDate = time.Date(2021, 05, 18, 0, 0, 0, 0, time.UTC).Unix()
+	case ConsensusV6:
+		cfg.EnableValidationSharding = true
+		cfg.Version = ConsensusV6
+		cfg.MigrationTimeout = 0
+		cfg.GenerateGenesisAfterUpgrade = true
 	}
 }
 
